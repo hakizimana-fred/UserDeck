898173fdee44fd4815fc5142264a4ed573910ea3
Hakizimana-fred <hakifred20@gmail.com>
2025-03-07 15:40:00 +0300

    wrap up

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -128,7 +128,7 @@ export function Modal({closeModal}: {closeModal: () => void}) {
                  
                     className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-emerald-600 text-base font-medium text-white hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 sm:ml-3 sm:w-auto sm:text-sm transition-colors duration-200"
                   >
-                    Add User
+                    {isUserEdit ? 'Update User' : 'Add User'}
                   </button>
                       </form>
                     </div>
